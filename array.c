5650f9825ae6f2d32dd950b9c326bd783ef08b1e
Danny <dchenviolinist@gmail.com>
2013-12-27 23:19:38 -0600

    Preliminary final product

--- array.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("{");
-    for(int i = 0; i < 9999; i++){
-        printf("%d, ", i);
-    }
-    printf("10000}");
-}
--- test.c
+++
@@ -1,6 +0,0 @@
-
-const unsigned char midi[100000] __attribute__((section("midi_file")));
-int main(){
-    return 0;
-}
-
